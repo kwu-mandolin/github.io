84c77bf1ceaafcab6f3d6e692ad52138b562ee75
kwu-mandolin <kwu.mandolin.orchestra@gmail.com>
2025-10-05 00:41:00 +0900

    Update main.js

--- index/main.js
+++ index/main.js
@@ -1,9 +1,29 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjIVvM5hz2q-A23mJeGQsN6xWhk54_cK8UJvpzgHX0mXBB_Yp3c6U4Ao_BN6kjk6b5IMfO7WJP0WIUQC__1GMgrO9IMYE75XbP-K7MgJPKMI00MzMGUV5vUuj8b7GkcXHktEcx-PKO4_we_4w0Vr9yESSbgbOlWqO8xtSYJ8fbHubuZeoqmyfM3s-g59iI7_E1Wak6plG851dIPy9WCOtU90lCXs1linhHT6PBxyAXn1xzpNmrLEadLGtk-ikjAY-NuEMWJpcy7362IsV2rK2FpU-1xig&lib=MKoaYyoemKuw1zE9Sj5faEbUz7p6ufxs_');
+request.open('GET', 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjIVvM5hz2q-A23mJeGQsN6xWhk54_cK8UJvpzgHX0mXBB_Yp3c6U4Ao_BN6kjk6b5IMfO7WJP0WIUQC__1GMgrO9IMYE75XbP-K7MgJPKMI[...]');
 request.responseType = 'json';
 request.onload = function () {
     let data = this.response;
-    console.log(data); 
-    document.getElementById('output').innerText = data['date'];
+    let table = `<table border="1"><thead><tr>
+        <th>楽器</th>
+        <th>使用者</th>
+        <th>購入年</th>
+        <th>メーカー</th>
+        <th>備品番号</th>
+        <th>メモ</th>
+    </tr></thead><tbody>`;
+    if (Array.isArray(data)) {
+        data.forEach(row => {
+            table += `<tr>
+                <td>${row.instrument || ""}</td>
+                <td>${row.user || ""}</td>
+                <td>${row.year || ""}</td>
+                <td>${row.maker || ""}</td>
+                <td>${row.itemId || ""}</td>
+                <td>${row.memo || ""}</td>
+            </tr>`;
+        });
+    }
+    table += "</tbody></table>";
+    document.getElementById('output').innerHTML = table;
 };
 request.send();
